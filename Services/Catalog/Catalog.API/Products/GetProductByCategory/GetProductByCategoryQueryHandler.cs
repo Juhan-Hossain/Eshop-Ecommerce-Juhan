58b364e1045f1b39f6fdcd9acdbb4bab1859b310
Juhan <juhan.hossain@brainstation-23.com>
2024-10-06 01:45:35 +0600

    Juhan: GetbyProductsCategory Done

---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string catogory) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> products);
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductByCategoryQueryHandler is called by {query}");
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.catogory))
+            .ToListAsync(cancellationToken);
+        return new GetProductByCategoryResult(products);
+    }
+}
+

bf054454ce5eb25996b90f07d1eb492429abf60a
Juhan <juhan.hossain@brainstation-23.com>
2024-10-14 15:33:18 +0600

    Juhan: Discount GRPC integrated into Shopping Cart

--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 public record StoreBasketResult(string UserName);
@@ -12,13 +14,18 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler(IBasketRepository basketRepository)
+public class StoreBasketCommandHandler
+    (IBasketRepository basketRepository, DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCart cart = command.Cart;
-        await basketRepository.StoreBasket(cart,cancellationToken);
+        foreach (var item in command.Cart.Items)
+        {
+            var coupon = await discountProtoServiceClient.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+        await basketRepository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketResult(command.Cart.UserName);
     }
 

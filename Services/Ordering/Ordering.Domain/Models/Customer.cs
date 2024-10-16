dd60d66c0bc4ac495368fbc943d2c6b534ab74d8
Juhan <juhan.hossain@brainstation-23.com>
2024-10-16 16:45:16 +0600

    Juhan: Rich Domain model for entity done

--- Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,10 +1,20 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models;
+public class Customer : Entity<CustomerId>
 {
-    public class Customer: Entity<Guid>
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public static Customer Create(CustomerId id, string name, string email)
     {
-        public string Name { get; private set; } = default!;
-        public string Email { get; private set; } = default!;
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+        var customer = new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+        return customer;
     }
 }
--- Services/Ordering/Ordering.Domain/Models/Order.cs
+++ Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,21 +1,22 @@
 ﻿using Ordering.Domain.Abstractions;
-using System.Net;
+using Ordering.Domain.Enums;
+using Ordering.Domain.ValueObjects;
+using Ordering.Domain.ValueObjects.CustomValueType;
 
 namespace Ordering.Domain.Models
 {
-    public class Order : Aggregate<Guid>
+    public class Order : Aggregate<OrderId>
     {
         private readonly List<OrderItem> _orderItems = new();
         private IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
 
-        public Guid CustomerId { get; private set; } = default!;
-        public string OrderName { get; private set; } = default!;
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
-        public OrderStatus Status { get; private set; } = default!;
-        public Guid CustomerId { get; private set; } = OrderStatus.Pending;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
         public decimal TotalPrice
         {
             get => OrderItems.Sum(x => x.Price * x.Quantity);

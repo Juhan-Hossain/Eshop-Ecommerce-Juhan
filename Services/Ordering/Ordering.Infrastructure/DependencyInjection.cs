526ff18f302892e6743922b5b36e22a4a7e53264
Juhan <juhan.hossain@brainstation-23.com>
2024-10-16 19:24:29 +0600

    Juhan: without migration everything done to commit

--- Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Infrastructure
+﻿namespace Ordering.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Database");
 
-            ////Add Services to the container
-            ///
+            // Add services to the container.
+            /*services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>()*/;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               // options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             return services;
         }
